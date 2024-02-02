7d69b320ff5219c450e1137caecea7f727f01760
ShanthakumarThanus <thanus.shanthakumar.sio@gmail.com>
2024-02-02 20:33:02 +0100

    modif navbar responsive

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -28,10 +31,23 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 
 /*==================== scroll reveal ====================*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duraction: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form' { origin:
+'bottom'});
+ScrollReveal().reveam('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveam('.home-content p, .about-content', {origin: 'right'});
 
 
 /*==================== typed js ====================*/
